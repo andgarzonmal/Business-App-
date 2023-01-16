0ffdfa03394e3f38f5940a19bd565a9883ae0b89
andgarzonmal <andgarzonmal@unal.edu.co>
2023-01-16 15:34:06 -0500

    Add some new styles to button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = () => {
   return (
-    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none mt-10`}>
+    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none mt-10 rounded-[10px]`}>
       Get Started
     </button>
   )
