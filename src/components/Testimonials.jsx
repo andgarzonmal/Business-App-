e69162103707ca660cc60c7ffb344427c70d0064
andgarzonmal <andgarzonmal@unal.edu.co>
2023-01-16 15:36:23 -0500

    Set up imports

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from 'react';
+import { feedback } from '../constants';
+import styles from '../style'
+import FeedbackCard from './FeedbackCard';
 
 const Testimonials = () => {
   return (
