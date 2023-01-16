e0f85646f6e1408df681b50acdcae6753cf3402f
andgarzonmal <andgarzonmal@unal.edu.co>
2023-01-16 15:22:42 -0500

    Set up imports

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from 'react';
+import { card } from '../assets';
+import styles, { layout } from '../style';
+import Button from './Button';
 
 const CardDeal = () => {
   return (
